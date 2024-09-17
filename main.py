6b5b151938e4e199184a2451c4030f7c89877d20
LupisCor <doliveri575@gmail.com>
2024-09-17 17:57:59 -0400

    group updateable and drawable objects

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import pygame.sprite
 from constants import *
 from player import Player
 
@@ -10,21 +11,27 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
-        player.update(dt)
+        for updateables in updateable:
+            updateables.update(dt)
+        
 
 
 
